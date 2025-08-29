ca88a14428fe236160bc84c7390068ec7ad7d5e4
Christoph Brauer <brauer@faktor-e.de>
2025-08-29 11:27:18 +0200

    UPDATE - adds favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index,follow">
+    <link rel="icon" href="{{ asset('favicon.png') }}" sizes="any">
 
     @stack('head')
 
